095979e84d94141be44e49826681807a3bef673c
Jose Miguel Bataller <jose@pragmati.co>
2018-05-18 19:13:15 +0100

    Added mock server

--- __tests__/routes/balances.test.js
+++ __tests__/routes/balances.test.js
@@ -1,14 +1,39 @@
-const balances = require('../../routes/balances');
-const getBalance = balances.getBalance
+const getBalance = require('../../routes/balances').getBalance;
+var nock = require('nock');
 
-it('get balance of an valid ETH address', () => {
-  process.env.PROVIDER_ENDPOINT = "https://api.blockcypher.com/v1/eth/main/addrs"
-  expect.assertions(1);
-  return getBalance("738d145faabb1e00cf5a017588a9c0f998318012").then(b => expect(b).toEqual(0.019254563))
-});
 
-it('get balance of an invalid ETH address', () => {
+describe('test get balance', () => {
+
   process.env.PROVIDER_ENDPOINT = "https://api.blockcypher.com/v1/eth/main/addrs"
-  expect.assertions(1);
-  return getBalance("invalid").then(b => expect(b).toEqual(NaN))
-});
+  nock('https://api.blockcypher.com')
+                .get(`/v1/eth/main/addrs/738d145faabb1e00cf5a017588a9c0f998318012/balance`)
+                .reply(200, {
+                  "address": "738d145faabb1e00cf5a017588a9c0f998318012",
+                  "total_received": 9762206505909057760,
+                  "total_sent": 9742951942909057760,
+                  "balance": 19254563000000000,
+                  "unconfirmed_balance": 0,
+                  "final_balance": 19254563000000000,
+                  "n_tx": 704,
+                  "unconfirmed_n_tx": 0,
+                  "final_n_tx": 704,
+                  "nonce": 414,
+                  "pool_nonce": 414
+                });
+
+  nock('https://api.blockcypher.com')
+                .get(`/v1/eth/main/addrs/invalid/balance`)
+                .reply(500, {"error": "Address 738d145faabb1e00cf5a017588a9c0f99831801x should be 40 is not hex encoded."}
+              );              
+
+  it('get balance of an valid ETH address', () => {
+    expect.assertions(1);
+    return getBalance("738d145faabb1e00cf5a017588a9c0f998318012").then(b => expect(b).toEqual(0.019254563))
+  });
+
+  it('get balance of an invalid ETH address', () => {
+    expect.assertions(1);
+    return getBalance("invalid").then(b => expect(b).toEqual(NaN))
+  });
+
+})
